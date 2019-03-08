f93c5ea6668f201bc34a36ab8ef82c3d7ac7091f
Coder101010101010101010 <02joseph.hill@gulllakecs.org>
2019-03-08 12:01:36 -0500

    Started part of challenge 1

---
+++ Final Exam.py
@@ -0,0 +1,15 @@
+# Joe Hill
+# Final Exam Part 1
+
+print ("This is a challenge game there are 5 Parts to this game the first part is try not to laugh ")
+
+print ("")
+
+print ("Try not to laugh challenge Part 1")
+
+print ("")
+
+print (" What type  of of meme do you like pick from these options pets, 2019, school, biology, civics, geometry, and ela.")
+
+meme_type = int(input("meme you want"))
+
