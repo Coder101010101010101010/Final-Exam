a3d4d07a933653bf54375a0ea6ad89814da70ce0
Coder101010101010101010 <48028297+Coder101010101010101010@users.noreply.github.com>
2019-03-12 18:24:55 -0400

    Update Final Exam.py

--- Final Exam.py
+++ Final Exam.py
@@ -1,2 +1,37 @@
 # Joe Hill
 # Final Exam Part 1
+
+print ("hope you like numbers")
+
+# For Loops
+# counting
+
+print ("do you like to count to 100? Well to bad if you don't")
+for i in range(101):
+    print(i)
+
+
+# while loops
+
+
+# break and continue
+# math problem
+
+
+print ("")
+
+print ('you will be asked one math problem')
+print ('1 a coder has 1000 ones and zeros he losses 330 ones and 99 zeros how many zeros and ones does he have left? ')
+
+while True:
+    answer = int(input('Enter an answer '))
+    if answer > 571:
+        print ("to high try again ")
+
+    elif answer <571:
+        print ("to low try again ")
+    
+    else:
+        print ("you got it right ")
+        break
+        
