3b5c0e9835f73d9c3c488203f26e50fba1930487
Coder101010101010101010 <02joseph.hill@gulllakecs.org>
2019-03-12 13:01:20 -0400

    restarted

--- Final Exam.py
+++ Final Exam.py
@@ -1,21 +1,2 @@
 # Joe Hill
 # Final Exam Part 1
-
-print ("This is a challenge game there are 5 Parts to this game the first part is try not to laugh ")
-
-print ("")
-
-print ("Try not to laugh challenge Part 1")
-
-print ("")
-
-print ("What type of of meme do you like pick from these options: animal, 2019, school, biology, civics, geometry, and ela.")
-dog = 1
-meme_type = [animal, str(2019), school, biology, civics, geometry, ela]
-
-meme_type_you_want = int(input("meme you want "))
-
-while meme_type_you_want != meme_type:
-    print ("not a valid option use one of these options memes: animal, 2019, school, biology, civics, geometry, and ela. ")
-
-animal =
