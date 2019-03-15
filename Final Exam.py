ba33bdff528d2054b92298ec23634253a82e357e
Coder101010101010101010 <02joseph.hill@gulllakecs.org>
2019-03-15 08:42:40 -0400

    While loops

--- Final Exam.py
+++ Final Exam.py
@@ -1,25 +1,3 @@
-'''
-Joe Hill
-Final Exam
-Numbers are Funner
-This program has multiple number games have fun!
-'''
-
-print ("Joe Hill")
-print ('')
-print ("Numbers are Funner")
-print ('')
-print ("hope you like numbers")
-
-# For Loops
-# counting
-
-print ('')
-print ("Do you like to count?")
-print ('')
-for i in range(1, 11):
-    print(i)
-
 
 # while loops
 # guess my number
@@ -38,53 +16,3 @@ while guess != my_number:
 
 print ("")
 print ("Good job you have guessed it")
-
-
-# functions
-# your age next year
-
-
-age1 = 1
-
-try:
-    age2 = int(input('Enter your age: '))
-except ValueError:
-    print('\n''That wasn\'t an integer for your age')
-
-age = age1 + age2
-
-print ('You will be ' + str(age) + ' years old next year')
-print ('')
-
-# Parameters/Arguments
-# counting 7 times
-
-def print_multiple_times(string, times):
-    for i in range(times):
-        print(string)
-
-print_multiple_times('numbers are funner!',7)
-
-# break and continue
-# math problem
-
-
-print ("")
-
-print ('you will be asked one math problem')
-print ('1 a coder has 1000 ones and zeros he losses 330 ones and 99 zeros how many zeros and ones does he have left? ')
-
-while True:
-    answer = int(input('Enter an answer '))
-    if answer > 571:
-        print ("to high try again ")
-
-    elif answer <571:
-        print ("to low try again ")
-    
-    else:
-        print ("you got it right ")
-        break
-        
-print ('')
-print ('You are done!')
