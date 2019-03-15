b376912531f6dfecb944ae93c5a623105d5cd04b
Coder101010101010101010 <02joseph.hill@gulllakecs.org>
2019-03-15 08:45:00 -0400

    changed stuff

--- Final Exam.py
+++ Final Exam.py
@@ -1,16 +1,3 @@
-'''
-Joe Hill
-Final Exam
-Numbers are Funner
-This program has multiple number games have fun!
-'''
-
-print ("Joe Hill")
-print ('')
-print ("Numbers are Funner")
-print ('')
-print ("hope you like numbers")
-
 # For Loops
 # counting
 
